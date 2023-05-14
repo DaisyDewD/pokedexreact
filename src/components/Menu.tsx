63f1196089fcac8c9cfdbb668dd68f420fe34792
“DaisyDew” <d.dew.design@gmail.com>
2023-05-14 02:16:13 -0500

    menu

---
+++ src/components/Menu.tsx
@@ -0,0 +1,27 @@
+import { Link } from "react-router-dom";
+import PokemonsButton from "../assets/pikachu.png";
+import ItemsButton from "../assets/pokeball.png";
+import LocationButton from "../assets/pointer.png";
+import styles from "./Menu.module.css";
+
+const Menu = () => {
+  return (
+    <menu className={styles.menu}>
+      <Link to="/pokemons" className={styles.menuLink}>
+        <img src={PokemonsButton} alt="PokemonsButton" className={styles.menuIcon}/>
+        Pokemons
+      </Link>
+
+      <Link to="/location" className={styles.menuLink}>
+        <img src={LocationButton} alt="LocationButton" className={styles.menuIcon}/>
+        Location
+      </Link>
+      <Link to="/items" className={styles.menuLink}>
+        <img src={ItemsButton} alt="ItemsButton"className={styles.menuIcon} />
+        Items
+      </Link>
+    </menu>
+  );
+};
+
+export default Menu;
--- src/pages/Pokemons.tsx
+++ src/pages/Pokemons.tsx
@@ -1,18 +1,17 @@
 //https://raw.githubusercontent.com/PokeAPI/media/master/logo/pokeapi_256.png
 import { useState } from 'react';
 import Header from '../components/Header';
+import Menu from '../components/Menu';
 
 const Pokemons = () => {
   const [query, setQuery] = useState('')
   return (
   <>
    <Header query={query} setQuery={setQuery}/>
+    <Menu/>
     <main>
     <h1>Pokemons</h1>
     </main>
-    <footer>
-        Pokes by DaisyDew
-    </footer>
   </>
   )
 };
